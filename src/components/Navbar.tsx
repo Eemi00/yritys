58414a5a8ec87bb361bbb30cd2ac68be105f6a29
Eemi00 <eemi.illik@gmail.com>
2025-12-15 21:59:09 +0200

    navbar valmis

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,5 +1,30 @@
+import { useState } from "react"
+import { Link } from "react-router-dom"
+import '../styles/Navbar.css'
+
 export default function Navbar() {
+
+    const [open, setOpen] = useState(false)
+
     return (
-        <h1>Navbar</h1>
+        <nav className="navbar">
+            <div className="navbar-logo">
+                <span>Yritys</span>
+            </div>
+
+            <button className="menu-toggle" onClick={() => setOpen(!open)} aria-label="Avaa/Sulje menu">
+                {open ? (
+                    <i className="fa-solid fa-xmark"></i>
+                ) : (
+                    <i className="fa-solid fa-bars"></i>
+                )}
+            </button>
+
+            <ul className={`navbar-links ${open ? 'show' : ''}`}>
+                <li><Link to="/" onClick={() => setOpen(false)}>Etusivu</Link></li>
+            </ul>
+
+            {open && <div className="menu-overlay" onClick={() => setOpen(false)}></div>}
+        </nav>
     )
 }
